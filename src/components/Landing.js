fe5c780538fc101eac53f8ebbb1363708bb721fa
Adole Samuel <cyberorgnizm@gmail.com>
2020-08-17 00:44:50 +0100

    💄 Feat: create more UI components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,78 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Layout from "./Layout";
 import Container from "@material-ui/core/Container";
-import { Button, TextField } from "@material-ui/core";
+import { Typography, Button, Grid, Box, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     height: "100vh",
   },
+  action: {
+    marginTop: "20px",
+  },
 }));
 
 function Landing() {
   const classes = useStyles();
-  const [message, setMessage] = useState("");
-  const [chats, setChats] = useState([]);
-  const [connection, setConnection] = useState({});
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    connection.send(message);
-    setMessage("");
-  };
-
-  const handleMessageChange = (e) => {
-    setMessage(e.target.value);
-  };
-
-  useEffect(() => {
-    let ws = new WebSocket(`ws://${window.location.host}/ws`);
-    // let ws = new WebSocket("wss://echo.websocket.org");
-    ws.onopen = (e) => {
-      setConnection(ws);
-    };
-  }, []);
-
-  connection.onmessage = (e) => {
-    const chatLog = [e.data, ...chats];
-    setChats(chatLog);
-  };
 
   return (
     <Layout>
       <main className={classes.root}>
-        <Container maxWidth="xs">
-          <form noValidate onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="message"
-              label="Message"
-              name="message"
-              autoFocus
-              value={message}
-              onChange={handleMessageChange}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <strong>{connection.readyState && "Connected successfully"}</strong>
-            <div>
-              {chats.map((chat) => (
-                <p>{chat}</p>
-              ))}
-            </div>
-          </form>
-        </Container>
+        <Box
+          minHeight="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Container maxWidth="sm">
+            <Typography variant="h4">
+              A real-time video streaming service
+            </Typography>
+            <Grid justify="center" container className={classes.action}>
+              <Button color="primary" variant="contained">
+                Get started
+              </Button>
+            </Grid>
+            <Grid container>
+              <Grid item sm={12}>
+                <form>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    id="stream_id"
+                    label="Join"
+                    placeholder="Copy and paste stream ID here to connect"
+                    name="stream_id"
+                    autoFocus
+                    disabled
+                  />
+                </form>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
       </main>
     </Layout>
   );
