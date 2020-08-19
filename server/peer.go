f874427c5dc6604f1a816f2dcd876139d7d52ee3
Adole Samuel <cyberorgnizm@gmail.com>
2020-08-19 10:18:09 +0100

    🚚  Chore: rename source file for connection

---
+++ server/peer.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+// Connection created for each new connection to the hub
+type Peer struct {
+	hub *Hub
+
+	// The websocket connection.
+	socketConnection *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	sink chan []byte
+}
